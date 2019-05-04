aafab0cf47d68f6dae09e7edbf68af4eec61bbf9
xiongxiaolong <xiaolongupc@163.com>
2019-05-04 15:20:56 +0800

    project init commit

---
+++ src/main/java/com/travelassistant/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.travelassistant.dao;
+
+import com.travelassistant.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
